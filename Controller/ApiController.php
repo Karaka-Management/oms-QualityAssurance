4aba0c4aa1a55751f53ca2d1abadc7fedc2cb137
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-03-17 21:35:01 +0000

    fix tests

---
+++ Controller/ApiController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   Modules\QualityAssurance
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\QualityAssurance\Controller;
+
+/**
+ * QualityAssurance api controller class.
+ *
+ * @package Modules\QualityAssurance
+ * @license OMS License 2.0
+ * @link    https://jingga.app
+ * @since   1.0.0
+ */
+final class ApiController extends Controller
+{
+}

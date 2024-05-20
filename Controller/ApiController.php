9c2fee96c1b95b2f7672981fb58a3cae1bdced7a
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-21 00:09:16 +0200

    fix version and bugs

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\QualityAssurance
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -18,7 +18,7 @@ namespace Modules\QualityAssurance\Controller;
  * QualityAssurance api controller class.
  *
  * @package Modules\QualityAssurance
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
--- Controller/Controller.php
+++ Controller/Controller.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\QualityAssurance
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -21,7 +21,7 @@ use phpOMS\Module\WebInterface;
  * QualityAssurance controller class.
  *
  * @package Modules\QualityAssurance
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
